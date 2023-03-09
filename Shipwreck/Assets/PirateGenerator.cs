bde33b952b93ff5764880bb1969f1f26da8aa885
juliferre09 <julianferreiraherrero@gmail.com>
2023-03-09 09:08:55 +0000

    update health bar and enemies generators

--- Shipwreck/Assets/PirateGenerator.cs
+++ Shipwreck/Assets/PirateGenerator.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 public class PirateGenerator : MonoBehaviour {
 
     public GameObject enemy;
-    public int xPos;
-    public int yPos;
+    private PlayerController player;
+    public float xPos;
+    public float yPos;
     public int pirateCount;
     public float spawnTime = 5f;
 
     private void Start() {
+        player = FindObjectOfType<PlayerController>();
         StartCoroutine(EnemyDrop());
     }
 
@@ -22,17 +24,19 @@ public class PirateGenerator : MonoBehaviour {
             if (Random.value > 0.5) 
             {
                 xPos = Random.Range(-15, 15);
-                if (Random.value > 0.5) { yPos = -8; }
-                else { yPos = 8; }
+                if (Random.value > 0.5) { yPos = -8f; }
+                else { yPos = 8f; }
             }
             else 
             {
-                if (Random.value > 0.5) { xPos = -15; }
-                else { xPos = 15; }
+                if (Random.value > 0.5) { xPos = -15f; }
+                else { xPos = 15f; }
                 yPos = Random.Range(-8, 8);
             }
+            xPos += player.transform.position.x;
+            yPos += player.transform.position.y;
             Instantiate(enemy, new Vector3(xPos, yPos, 0), Quaternion.identity);
-            Debug.Log("Spawned at: (" + xPos + "," + yPos + ")");
+            Debug.Log("Pirate spawned at: (" + xPos + "," + yPos + ")");
             yield return new WaitForSeconds(spawnTime);
             pirateCount += 1;
         }
--- Shipwreck/Assets/SharkGenerator.cs
+++ Shipwreck/Assets/SharkGenerator.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 public class SharkGenerator : MonoBehaviour {
 
     public GameObject enemy;
-    public int xPos;
-    public int yPos;
+    private PlayerController player;
+    public float xPos;
+    public float yPos;
     public int sharkCount;
     public float spawnTime = 5f;
 
     private void Start() {
+        player = FindObjectOfType<PlayerController>();
         StartCoroutine(EnemyDrop());
     }
 
@@ -22,17 +24,19 @@ public class SharkGenerator : MonoBehaviour {
             if (Random.value > 0.5) 
             {
                 xPos = Random.Range(-15, 15);
-                if (Random.value > 0.5) { yPos = -8; }
-                else { yPos = 8; }
+                if (Random.value > 0.5) { yPos = -8f; }
+                else { yPos = 8f; }
             }
             else 
             {
-                if (Random.value > 0.5) { xPos = -15; }
-                else { xPos = 15; }
+                if (Random.value > 0.5) { xPos = -15f; }
+                else { xPos = 15f; }
                 yPos = Random.Range(-8, 8);
             }
+            xPos += player.transform.position.x;
+            yPos += player.transform.position.y;
             Instantiate(enemy, new Vector3(xPos, yPos, 0), Quaternion.identity);
-            Debug.Log("Spawned at: (" + xPos + "," + yPos + ")");
+            Debug.Log("Shark spawned at: (" + xPos + "," + yPos + ")");
             yield return new WaitForSeconds(spawnTime);
             sharkCount += 1;
         }
